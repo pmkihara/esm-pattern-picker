fe24a0c546c734b06684de7e33ef160c4062938a
Patricia Kihara <patricia.kihara@outlook.com>
2025-01-17 16:40:25 -0300

    feat: show custom job without the group

--- src/components/molecules/JobSearch/index.tsx
+++ src/components/molecules/JobSearch/index.tsx
@@ -13,12 +13,15 @@ import JobGroup from '../JobGroup'
 import {
   CommandEmpty,
   CommandInputAsChild,
+  CommandItem,
   CommandMenu,
 } from '@/components/atoms/CommandMenu'
 import WorkIcon from '@@/public/assets/icons/work.svg'
 import Popover from '@/components/atoms/Popover'
 import { useState } from 'react'
 import Input from '@/components/atoms/Input'
+import LightstickIcon from '@@/public/assets/icons/lightstick.svg'
+import Image from 'next/image'
 
 interface JobSearchProps {
   selectedJob?: OfficeJob
@@ -29,14 +32,13 @@ const JobSearch = ({ selectedJob, setOfficeJob }: JobSearchProps) => {
   const [open, setIsOpen] = useState(false)
   const [search, setSearch] = useState('')
 
-  const officeJobs: Record<OfficeJobGroup, OfficeJob[]> = {
+  const officeJobs: Partial<Record<OfficeJobGroup, OfficeJob[]>> = {
     [OfficeJobGroup.Primary]: primaryJobs,
     [OfficeJobGroup.Secondary]: secondaryJobs,
     [OfficeJobGroup.Advanced]: advancedJobs,
     [OfficeJobGroup.Expert]: expertJobs,
     [OfficeJobGroup.Unit]: unitJobs,
     [OfficeJobGroup.EsWork]: esJobs,
-    [OfficeJobGroup.Custom]: [emptyJob],
   }
 
   const input = (
@@ -73,6 +75,21 @@ const JobSearch = ({ selectedJob, setOfficeJob }: JobSearchProps) => {
             onJobSelect={onJobSelect}
           />
         ))}
+        <CommandItem
+          value={emptyJob.name}
+          className='text-xs'
+          onSelect={() => onJobSelect(emptyJob)}
+        >
+          <div className='flex gap-2 items-center'>
+            <Image
+              src={LightstickIcon.src}
+              alt='custom'
+              width={16}
+              height={16}
+            />
+            <p className='text-sm'>{emptyJob.name}</p>
+          </div>
+        </CommandItem>
         <CommandEmpty />
       </Popover>
     </CommandMenu>
